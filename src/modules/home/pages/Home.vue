d6f9f8d6e19e9923b44aaf658b173e52bcc251b6
陈家敬 <jiajingchen@xiaoe-tech.com>
2020-11-07 02:17:16 +0800

    init

---
+++ src/modules/home/pages/Home.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="@/assets/logo.png" />
+    <HelloWorld :msg="'测试vuex'"></HelloWorld>
+    <button @click="add">+</button>
+    &nbsp;&nbsp;&nbsp;&nbsp;
+    <button @click="de">-</button>
+    <div>
+      <p>{{ count }}</p>
+      <hr />
+      <p>{{ countGetters }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState, mapGetters, mapMutations, mapActions } from "vuex";
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  components: {
+    HelloWorld,
+  },
+  computed: {
+    ...mapState({ count: (state) => state.count }),
+    ...mapGetters({
+      countGetters: "countGetters",
+    }),
+  },
+  methods: {
+    ...mapMutations({
+      increment: "increment",
+      decrement: "decrement",
+    }),
+    ...mapActions({}),
+
+    add() {
+      // this.increment(1);
+      this.$store.commit("increment", 1);
+      console.log(this.count);
+    },
+    de() {
+      // this.decrement(1);
+      this.$store.commit("decrement", 1);
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+button {
+  width: 50px;
+  height: 30px;
+  cursor: pointer;
+}
+</style>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,51 @@
 import Vue from "vue";
-import Vuex from "vuex";
+import Vuex, { GetterTree, MutationTree, ActionTree } from "vuex";
+
+import home from "./home";
+import about from "./about";
 
 Vue.use(Vuex);
+const debug = process.env.Node_ENV !== "production";
 
-export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+export interface RootState {
+  count: number;
+}
+
+// state
+const state: RootState = {
+  count: 0,
+};
+
+// getters
+const getters: GetterTree<RootState, RootState> = {
+  countGetters(state): string {
+    return `当前count的值为：${state.count}`;
+  },
+};
+
+//mutations
+const mutations: MutationTree<RootState> = {
+  increment(state, step = 1) {
+    state.count += step;
+  },
+  decrement(state, step = 1) {
+    state.count -= step;
+  },
+};
+
+// actions
+const actions: ActionTree<RootState, RootState> = {};
+
+const store = new Vuex.Store({
+  state,
+  getters,
+  mutations,
+  actions,
+  modules: {
+    home,
+    about,
+  },
+  strict: debug,
 });
+
+export default store;
